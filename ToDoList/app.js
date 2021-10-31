c3b329ecdc954c8245988ce5128dc8e3c68ed264
FateOce <90393820+FateOce@users.noreply.github.com>
2021-10-31 14:49:24 +1100

    Update

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -2,10 +2,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
+
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 
 //functions
@@ -40,8 +43,11 @@ function deleteCheck(e){
     //DELETE TODO
     if(item.classList[0] === 'trash-btn'){
         const todo = item.parentElement;
+        //Animation
         todo.classList.add('fall');
-
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
     }
 
     //CHECK MARK
@@ -51,3 +57,36 @@ function deleteCheck(e){
     }
 
 }
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains('completed')){
+                    todo.style.display = "flex";
+                }else{
+                    todo.style.display = "none";
+                }
+            case "uncompleted":
+                if(!todo.classList.contains('completed')){
+                    todo.style.display = "flex";
+                }else{
+                    todo.style.display = "none";
+                }
+        
+            
+                
+
+        }
+    
+    
+    });
+
+
+
+}
+
+
